dbaeef81b971d7d519ed30209539e1b37190dddf
Alex Chopard <alex.chopard1998@gmail.com>
2018-12-06 21:11:36 +0100

    End localisation endpoint

--- src/controllers/localisationController.js
+++ src/controllers/localisationController.js
@@ -5,18 +5,28 @@ import mongoose from 'mongoose'
 const Localisation = mongoose.model('Localisation')
 
 exports.listAllLocalisations = function(req, res) {
-  Localisation.find({}, function(err, task) {
-    if (err)
-      res.send(err)
-    res.json(task)
+  Localisation.find({}, function(err, localisations) {
+    if (err) {
+      res.status(500).send(err)
+    }
+
+    res.status(200).json(localisations)
   })
 }
 
-exports.createLocalisation = function(req, res) {
-  var newLocalisation = new Localisation(req.body)
-  newLocalisation.save(function(err, task) {
-    if (err)
-      res.send(err)
-    res.json(task)
-  })
+exports.createLocalisation = (req, res) => {
+  const body = req.body
+
+  if (body && Object.keys(body).length === 2 && body.longitude && body.latitude) {
+    const newLocalisation = new Localisation(req.body)
+    newLocalisation.save((err, localisation) => {
+      if (err) {
+        res.status(500).send(err)
+      }
+
+      res.status(200).json(localisation)
+    })
+  } else {
+    res.status(400).json({ message: 'Invalid params !' })
+  }
 }
